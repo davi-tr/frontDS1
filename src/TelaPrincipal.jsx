fdba16798858b2b16525c1dec1eb2c343a90df32
Bryene <bryenev@gmail.com>
2023-08-29 22:31:35 -0300

    botão de excluir pesquisador e afins.

--- src/TelaPrincipal.jsx
+++ src/TelaPrincipal.jsx
@@ -1,16 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import DataTable from './components/Instituto/DataTable';
-import AddResearcherForm from './components/Instituto/AddResearcherForm'; // Importe o formulário de adição de pesquisador aqui
+import AddResearcherForm from './components/Instituto/AddResearcherForm';
 import axios from 'axios';
-
-
 
 function TelaPrincipal() {
   const [mostrarDataTable, setMostrarDataTable] = useState(false);
   const [pesquisadores, setPesquisadores] = useState([]);
+  const [selectedPesquisador, setSelectedPesquisador] = useState(null);
 
   useEffect(() => {
-    fetchPesquisadores(); // Busca a lista de pesquisadores ao carregar o componente
+    fetchPesquisadores();
   }, []);
 
   const fetchPesquisadores = async () => {
@@ -23,39 +22,59 @@ function TelaPrincipal() {
   };
 
   const handleVoltarParaTelaPrincipal = () => {
-    window.location.href = "/"; // Navegação simples para a raiz do aplicativo
-};
+    window.location.href = "/";
+  };
+
+  const handleDeleteClick = async (pesquisadorId) => {
+    try {
+      await axios.delete(`http://localhost:8081/pesquisador/${pesquisadorId}`);
+      // Atualizar a lista de pesquisadores após a exclusão
+      fetchPesquisadores();
+      setSelectedPesquisador(null); // Limpar a seleção
+    } catch (error) {
+      console.error('Erro ao excluir o pesquisador:', error);
+    }
+  };
+
+  const handlePesquisadorSelect = (pesquisador) => {
+    setSelectedPesquisador(pesquisador);
+  };
 
   return (
     <div>
-       <button onClick={handleVoltarParaTelaPrincipal}>Voltar para a Tela Principal</button>
+      <button onClick={handleVoltarParaTelaPrincipal}>Voltar para a Tela Principal</button>
 
-      {/* Renderiza o formulário de adição de pesquisador */}
       <AddResearcherForm onClose={() => setMostrarDataTable(false)} />
-        <div className="container" style={{ width: '100%', height: '70vh', overflow: 'auto' }}>
-          <h2 className="titulo">Pesquisadores Cadastrados</h2>
+      <div className="container" style={{ width: '100%', height: '70vh', overflow: 'auto' }}>
+        <h2 className="titulo">Pesquisadores Cadastrados</h2>
         <table className="data-table-pesquisadores">
           <thead>
             <tr>
               <th>ID</th>
               <th>Nome</th>
               <th>Instituto</th>
-              {/* Mais colunas aqui, exceto e-mail */}
             </tr>
           </thead>
           <tbody>
-            {pesquisadores.map(pesquisador => (
-              <tr key={pesquisador.id}>
+            {pesquisadores.map((pesquisador) => (
+              <tr key={pesquisador.id} onClick={() => handlePesquisadorSelect(pesquisador)}>
                 <td>{pesquisador.id}</td>
                 <td>{pesquisador.nome}</td>
                 <td>{pesquisador.instituto.nome}</td>
-                {/* Mais colunas aqui, exceto e-mail */}
               </tr>
             ))}
           </tbody>
         </table>
-        </div>
-      </div>  
+      </div>
+
+      <div className="edit-delete-buttons">  <button
+        className="delete-button-2"
+        disabled={!selectedPesquisador}
+        onClick={() => handleDeleteClick(selectedPesquisador.id)}
+      >  Excluir
+      </button>
+      </div>
+    </div>
   );
 }
 
