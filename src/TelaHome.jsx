19e7d3c0628df560bd4d10a55f0baf68386d646f
Davi Trajano <davitrajano473@gmail.com>
2023-10-16 17:27:35 -0300

    Nova estilizaçao

--- src/TelaHome.jsx
+++ src/TelaHome.jsx
@@ -4,11 +4,10 @@ import TelaPrincipal from './TelaPrincipal';
 import DataTable from './components/Instituto/DataTable';
 import fotofemass from './fotofemass.png';
 import TelaProducoes from './TelaProducoes';
-
+import './TelaHome.css'; // Importe o arquivo de estilos atualizado
 
 function Home() {
   const [currentScreen, setCurrentScreen] = useState('home');
-  
 
   const handleNavigate = (screen) => {
     setCurrentScreen(screen);
@@ -19,14 +18,21 @@ function Home() {
       <div className="sidebar">
         <h2>Menu</h2>
         <ul>
-          <li onClick={() => handleNavigate('home')}>Home</li>
-          <li onClick={() => handleNavigate('pesquisador')}>Pesquisadores</li>
-          <li onClick={() => handleNavigate('instituto')}>Institutos</li>
-          <li onClick={() => handleNavigate('producoes')}>Produções</li>
+          <li className={currentScreen === 'home' ? 'active' : ''} onClick={() => handleNavigate('home')}>
+            Home
+          </li>
+          <li className={currentScreen === 'pesquisador' ? 'active' : ''} onClick={() => handleNavigate('pesquisador')}>
+            Pesquisadores
+          </li>
+          <li className={currentScreen === 'instituto' ? 'active' : ''} onClick={() => handleNavigate('instituto')}>
+            Institutos
+          </li>
+          <li className={currentScreen === 'producoes' ? 'active' : ''} onClick={() => handleNavigate('producoes')}>
+            Produções
+          </li>
         </ul>
       </div>
       <div className="content">
-      
         {currentScreen === 'pesquisador' && <TelaPrincipal />}
         {currentScreen === 'instituto' && <DataTable />}
         {currentScreen === 'home' && (
@@ -41,7 +47,6 @@ function Home() {
           </div>
         )}
         {currentScreen === 'producoes' && <TelaProducoes />}
-
       </div>
     </div>
   );
