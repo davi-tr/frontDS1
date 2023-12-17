7c871355e77f55c1b1894c2447b76b2b50fd3d3f
Davi Trajano <90059259+davi-tr@users.noreply.github.com>
2023-12-17 01:37:41 -0300

    Depuração tela grafos

--- src/Grafos.jsx
+++ src/Grafos.jsx
@@ -15,22 +15,44 @@ const GraphComponent = ({ colorRanges, edgeType, vertexType }) => {
   };
 
   useEffect(() => {
-    // Lógica para buscar dados JSON
-    // ...
+    // Inicia o carregamento
+    setLoading(true);
+
+    // Faz a requisição para a API
+    fetch('http://localhost:8083/producao')
+      .then(response => {
+        // Verifica se a resposta é ok
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        // Converte a resposta para JSON
+        return response.json();
+      })
+      .then(data => {
+        // Atualiza o estado com os dados JSON
+        setJsonData(data);
+        // Finaliza o carregamento
+        setLoading(false);
+        console.log(jsonData)
+      })
+      .catch(error => {
+        console.error('There has been a problem with your fetch operation:', error);
+      });
   }, []);
 
   useEffect(() => {
     if (!jsonData) {
+      console.log('No data');
       return;
     }
 
     // Verifique se o elemento do contêiner está disponível
     const container = graphContainerRef.current;
     if (!container) {
+      console.log('No container');
       return;
     }
 
-    // Limpe a instância da rede existente, se houver
     if (networkRef.current) {
       networkRef.current.destroy();
     }
@@ -41,16 +63,18 @@ const GraphComponent = ({ colorRanges, edgeType, vertexType }) => {
     // Crie arestas com base no relacionamento entre pesquisadores e suas produções
     const edges = new DataSet();
 
-    jsonData.forEach(item => {
+    jsonData.content.forEach(item => {
       const researchers = item[vertexType] || [];
 
       // Adicione pesquisadores ao conjunto
       researchers.forEach(researcher => {
         if (!researcherNodes.get(researcher.id)) {
-          // Altere a cor do nó com base no número de produções
           let nodeColor = 'white'; // Cor padrão
           const productionsCount = researcherProductionsCount(researcher.id);
-
+      
+          console.log('productionsCount:', productionsCount); // Adicione esta linha
+          console.log('colorRanges:', colorRanges); // Adicione esta linha
+      
           // Determine a cor do nó com base nos intervalos de cor fornecidos (colorRanges)
           for (const range of colorRanges) {
             if (productionsCount >= range.start && productionsCount <= range.end) {
@@ -58,7 +82,7 @@ const GraphComponent = ({ colorRanges, edgeType, vertexType }) => {
               break;
             }
           }
-
+      
           researcherNodes.add({
             id: researcher.id,
             label: researcher.nome,
@@ -66,7 +90,7 @@ const GraphComponent = ({ colorRanges, edgeType, vertexType }) => {
           });
         }
       });
-
+      
       // Crie arestas entre pesquisadores se houver vários pesquisadores
       for (let i = 0; i < researchers.length - 1; i++) {
         for (let j = i + 1; j < researchers.length; j++) {
@@ -80,12 +104,17 @@ const GraphComponent = ({ colorRanges, edgeType, vertexType }) => {
       }
     });
 
+    
+
     const data = {
       nodes: researcherNodes,
       edges: edges,
     };
+    console.log(researcherNodes)
+  
 
     const options = {};
+    
 
     // Crie uma nova instância da rede
     const network = new Network(container, data, options);
@@ -101,13 +130,13 @@ const GraphComponent = ({ colorRanges, edgeType, vertexType }) => {
       }
     };
   }, [jsonData, colorRanges, edgeType, vertexType]);
-
+  console.log("rede: " + networkRef.current)
   return (
     <div>
       {loading ? (
         <p>Loading...</p>
       ) : (
-        <div ref={graphContainerRef} style={{ height: '900px' }}></div>
+        <div ref={graphContainerRef} style={{ height: '900px'}}></div>
       )}
     </div>
   );
